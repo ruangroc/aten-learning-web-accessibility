9e1b5eb3f8906f2483e1a1b232e2882bdae4cb87
Chalida (Anita) Ruangrotsakun <ruangroc@oregonstate.edu>
2023-05-13 14:32:35 -0700

    added a form for signing up for a newsletter

--- basic-app/src/routes/Home.js
+++ basic-app/src/routes/Home.js
@@ -1,6 +1,6 @@
 import Navigation from "../components/Navigation";
 
-import { Container } from "react-bootstrap";
+import { Container, Form, Button, Card } from "react-bootstrap";
 
 export default function Home() {
     return (
@@ -12,6 +12,42 @@ export default function Home() {
                     Currently learning and applying web accessibility guidelines while creating 
                     a fictional pet store e-commerce site using React and React-Bootstrap.
                 </p>
+
+                <Card style={{ width: '28rem' }} border="info">
+                    <Card.Header>Get Notified!</Card.Header>
+                    <Card.Body>
+                        <Card.Title>Sign up for our newsletter!</Card.Title>
+                        <Card.Text>You'll receive promo codes and a special treat for you and your furry friend on your birthday!</Card.Text>
+
+                        <Form>
+                            <Form.Group className="mb-3" controlId="name">
+                                <Form.Label>What is your name?</Form.Label>
+                                <Form.Control type="text" placeholder="Enter name" />
+                            </Form.Group>
+
+                            <Form.Group className="mb-3" controlId="email">
+                                <Form.Label>What is your email address?</Form.Label>
+                                <Form.Control type="email" placeholder="Enter email" />
+                                <Form.Text className="text-muted">
+                                We'll never share your email with anyone else.
+                                </Form.Text>
+                            </Form.Group>
+
+                            <Form.Group className="mb-3" controlId="birthday">
+                                <Form.Label>When is your birthday?</Form.Label>
+                                <Form.Control type="date" placeholder="Enter birthday" />
+                                <Form.Text className="text-muted">
+                                We'll never share your birthday with anyone else.
+                                </Form.Text>
+                            </Form.Group>
+
+                            <Button variant="primary" type="submit">
+                                Submit
+                            </Button>
+                        </Form>
+                    </Card.Body>
+                </Card>
+                
             </Container>
         </div>
     );
